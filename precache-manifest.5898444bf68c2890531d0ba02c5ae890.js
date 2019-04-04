3760aef221e65a8f30372a1ce633dc58de6b315c
Jakub Smyk <jdsmyk@student.uw.edu.pl>
2019-04-05 01:19:04 +0200

    Updates

---
+++ precache-manifest.5898444bf68c2890531d0ba02c5ae890.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa5898297757e845812f",
+    "url": "/NowyLicznik/static/js/runtime~main.144cbfda.js"
+  },
+  {
+    "revision": "af7c4f18cfe0ed6d2c30",
+    "url": "/NowyLicznik/static/js/main.1c25154d.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/NowyLicznik/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "af7c4f18cfe0ed6d2c30",
+    "url": "/NowyLicznik/static/css/main.c2c17791.chunk.css"
+  },
+  {
+    "revision": "f6886668ac22d8eb2de02cd58c1360d2",
+    "url": "/NowyLicznik/index.html"
+  }
+];
